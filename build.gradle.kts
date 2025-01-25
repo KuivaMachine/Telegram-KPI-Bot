69796059d50b74ae4756016046d76e6f230f86dd
olegz <OLEGZAOSTROVTSEV19@yandex.ru>
2025-01-25 19:00:41 +0300

    2 modeles

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,28 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.4.1"
-    id("io.spring.dependency-management") version "1.1.7"
+	java
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(23)
-    }
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.google.api-client:google-api-client:2.7.0")
-    implementation ("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
-    implementation ("com.google.apis:google-api-services-sheets:v4-rev20220927-2.0.0")
-    implementation("org.postgresql:postgresql:42.7.5")
-    implementation("com.google.auth:google-auth-library-oauth2-http:1.30.1")
-    compileOnly("org.projectlombok:lombok")
-    implementation("org.springframework.kafka:spring-kafka")
-    runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation ("org.telegram:telegrambots:6.9.7.1")
-    implementation ("org.hibernate.validator:hibernate-validator:8.0.1.Final")
-    implementation ("org.apache.logging.log4j:log4j-core:2.24.3")
-    implementation("org.apache.logging.log4j:log4j-api:2.24.3")
-
-
-
-
-
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
