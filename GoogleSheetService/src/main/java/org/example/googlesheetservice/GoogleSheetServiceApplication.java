3a349536db9fcc3b337942ea0fdaaf42eda0c12b
olegz <OLEGZAOSTROVTSEV19@yandex.ru>
2025-02-05 20:21:23 +0300

    Обновлен класс GoogleSheetsService, все методы переделаны для минимизации количества запросов обновления таблицы

--- GoogleSheetService/src/main/java/org/example/googlesheetservice/GoogleSheetServiceApplication.java
+++ GoogleSheetService/src/main/java/org/example/googlesheetservice/GoogleSheetServiceApplication.java
@@ -1,14 +1,19 @@
 package org.example.googlesheetservice;
 
+import org.example.googlesheetservice.SheetsServices.GoogleSheetsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+
+
+
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.example.postgresql","org.example.googlesheetservice"})
+@ComponentScan(basePackages = {"org.example.postgresql", "org.example.googlesheetservice"})
+@EnableScheduling
 public class GoogleSheetServiceApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(GoogleSheetServiceApplication.class, args);
