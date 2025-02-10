5be7b6faa93e19905a2d67e25bbf3092aed8499b
olegz <OLEGZAOSTROVTSEV19@yandex.ru>
2025-02-10 23:22:21 +0300

    Новый коммит

--- src/main/java/org/example/kpitelegrambot/bot/handlers/Handler.java
+++
@@ -1,8 +0,0 @@
-package org.example.kpitelegrambot.bot.handlers;
-
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-public interface Handler {
-    SendMessage process(Update update);
-}
