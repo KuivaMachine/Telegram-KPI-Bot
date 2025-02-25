c1081b6010b62a60d166ab91feda1ff4b2b83686
olegz <OLEGZAOSTROVTSEV19@yandex.ru>
2025-02-25 19:57:34 +0300

    WebHooh_2

--- GoogleSheetService/src/main/java/org/example/googlesheetservice/GoogleSheetServiceApplication.java
+++ GoogleSheetService/src/main/java/org/example/googlesheetservice/GoogleSheetServiceApplication.java
@@ -1,6 +1,7 @@
 package org.example.googlesheetservice;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 
+@Log4j2
 @SpringBootApplication
 @EnableScheduling
 public class GoogleSheetServiceApplication {
@@ -17,6 +19,7 @@ public class GoogleSheetServiceApplication {
         Dotenv dotenv = Dotenv.configure().load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(GoogleSheetServiceApplication.class, args);
+        log.info("НОВАЯ ВЕРСИЯ_1");
     }
 
 }
